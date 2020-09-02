5b8d62fd997f0a51c29f338625a72b87627b30c4
DOJIN <djyou128@gmail.com>
2020-09-02 23:38:55 +0900

    practice for array function

--- array.js
+++ array.js
@@ -1,20 +1,20 @@
 // Array
 
- // 1. Declaration
+// 1. Declaration
 const arr1 = new Array();
-const arr2 = [1,2];
+const arr2 = [1, 2];
 
 // 2. Index position
-const fruits = ['apple','banana'];
-console.log(fruits.length) // 2
-console.log(fruits[0]) // apple
-console.log(fruits[1]) // banana
-console.log(fruits[2]) // undefined
+const fruits = ['apple', 'banana'];
+console.log(fruits.length); // 2
+console.log(fruits[0]); // apple
+console.log(fruits[1]); // banana
+console.log(fruits[2]); // undefined
 
 //3. Looping over an array
 // print all fruits
 // a. for
-for (let i=0; i<fruits.length; i++) {
+for (let i = 0; i < fruits.length; i++) {
 	console.log(fruits[i]);
 }
 // b. for of
@@ -22,37 +22,37 @@ for (let fruit of fruits) {
 	console.log(fruit);
 }
 // c. forEach
-fruits.forEach(function(fruit, index) => console.log(fruit));
+fruits.forEach((fruit, index) => console.log(fruit));
 
 // 4. Addtion, deletion, copy
 
 // push: add an item to the end
-fruits.push('strawberry','orange');
+fruits.push('strawberry', 'orange');
 console.log(fruits);
 // pop: remove an item from the end
 fruits.pop();
 fruits.pop();
 console.log(fruits);
 // unshift: add an item to the beningging
-fruits.unshift('strawberry','orange');
+fruits.unshift('strawberry', 'orange');
 console.log(fruits);
 // shift: remove an item to the beningging
-fruits.shift('strawberry','orange');
+fruits.shift('strawberry', 'orange');
 console.log(fruits);
 // shift와 unshift는 push와 pop보다 훨씬 느림
 
 // 5. slice, concat
 // splice(start, deleteCount?)
-fruits.splice(1,1);
+fruits.splice(1, 1);
 // concat(...items)
 fruits.concat(['lemon']);
 
 // 6. Searching
 // find the index
 // indexOf
-console.log(fruits.indexOf('apple');
+console.log(fruits.indexOf('apple'));
 // includes
-console.log(fruits.includes('apple'); // 없으면 -1
+console.log(fruits.includes('apple')); // 없으면 -1
 //lastIndexOf
 fruits.push('apple');
 console.log(fruits.lastIndexOf('apple'));
